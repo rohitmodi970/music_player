88016e4c4ba5ee30fc248d83e8ba07ba1d2db4d0
ROHIT MODI <132802604+rohitmodi970@users.noreply.github.com>
2024-08-26 15:27:56 +0530

    Update script.js

--- java_script/script.js
+++ java_script/script.js
@@ -21,7 +21,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:3000/PROJECT2%20-SPOTIFY%20CLONE/${folder}/`);
+    let a = await fetch(`/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
